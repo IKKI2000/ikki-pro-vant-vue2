010be3f181d69713399ac941f67083ad5155a3fd
IKKI2000 <ikki2002@qq.com>
2023-02-09 09:45:33 +0800

    【新增】配置 Vant UI 框架按需加载~

---
+++ src/plugins/vant.js
@@ -0,0 +1,180 @@
+import Vue from 'vue';
+import {
+    ActionSheet,
+    AddressEdit,
+    AddressList,
+    Area,
+    Badge,
+    Button,
+    Calendar,
+    Card,
+    Cascader,
+    Cell,
+    CellGroup,
+    Checkbox,
+    CheckboxGroup,
+    Circle,
+    Col,
+    Collapse,
+    CollapseItem,
+    ContactCard,
+    ContactEdit,
+    ContactList,
+    CountDown,
+    Coupon,
+    CouponCell,
+    CouponList,
+    DatetimePicker,
+    Dialog,
+    Divider,
+    DropdownItem,
+    DropdownMenu,
+    Empty,
+    Field,
+    Form,
+    GoodsAction,
+    GoodsActionButton,
+    GoodsActionIcon,
+    Grid,
+    GridItem,
+    Icon,
+    Image as VanImage,
+    ImagePreview,
+    IndexAnchor,
+    IndexBar,
+    Info,
+    Lazyload,
+    List,
+    Loading,
+    NavBar,
+    NoticeBar,
+    Notify,
+    NumberKeyboard,
+    Overlay,
+    Pagination,
+    // Panel,
+    PasswordInput,
+    Picker,
+    Popover,
+    Popup,
+    Progress,
+    PullRefresh,
+    Radio,
+    RadioGroup,
+    Rate,
+    Row,
+    Search,
+    ShareSheet,
+    Sidebar,
+    SidebarItem,
+    Skeleton,
+    Sku,
+    Slider,
+    Step,
+    Stepper,
+    Steps,
+    Sticky,
+    SubmitBar,
+    Swipe,
+    SwipeCell,
+    SwipeItem,
+    Switch,
+    // SwitchCell,
+    Tab,
+    Tabbar,
+    TabbarItem,
+    Tabs,
+    Tag,
+    Toast,
+    TreeSelect,
+    Uploader,
+} from 'vant';
+
+Vue.use(ActionSheet);
+Vue.use(AddressEdit);
+Vue.use(AddressList);
+Vue.use(Area);
+Vue.use(Badge);
+Vue.use(Button);
+Vue.use(Calendar);
+Vue.use(Card);
+Vue.use(Cascader);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(Circle);
+Vue.use(Col);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(ContactCard);
+Vue.use(ContactEdit);
+Vue.use(ContactList);
+Vue.use(CountDown);
+Vue.use(Coupon);
+Vue.use(CouponCell);
+Vue.use(CouponList);
+Vue.use(DatetimePicker);
+Vue.use(Dialog);
+Vue.use(Divider);
+Vue.use(DropdownItem);
+Vue.use(DropdownMenu);
+Vue.use(Empty);
+Vue.use(Field);
+Vue.use(Form);
+Vue.use(GoodsAction);
+Vue.use(GoodsActionButton);
+Vue.use(GoodsActionIcon);
+Vue.use(Grid);
+Vue.use(GridItem);
+Vue.use(Icon);
+Vue.use(VanImage);
+Vue.use(ImagePreview);
+Vue.use(IndexAnchor);
+Vue.use(IndexBar);
+Vue.use(Info);
+Vue.use(Lazyload);
+Vue.use(List);
+Vue.use(Loading);
+Vue.use(NavBar);
+Vue.use(NoticeBar);
+Vue.use(Notify);
+Vue.use(NumberKeyboard);
+Vue.use(Overlay);
+Vue.use(Pagination);
+// Vue.use(Panel);
+Vue.use(PasswordInput);
+Vue.use(Picker);
+Vue.use(Popover);
+Vue.use(Popup);
+Vue.use(Progress);
+Vue.use(PullRefresh);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(Rate);
+Vue.use(Row);
+Vue.use(Search);
+Vue.use(ShareSheet);
+Vue.use(Sidebar);
+Vue.use(SidebarItem);
+Vue.use(Skeleton);
+Vue.use(Sku);
+Vue.use(Slider);
+Vue.use(Step);
+Vue.use(Stepper);
+Vue.use(Steps);
+Vue.use(Sticky);
+Vue.use(SubmitBar);
+Vue.use(Swipe);
+Vue.use(SwipeCell);
+Vue.use(SwipeItem);
+Vue.use(Switch);
+// Vue.use(SwitchCell);
+Vue.use(Tab);
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+Vue.use(Tabs);
+Vue.use(Tag);
+Vue.use(Toast);
+Vue.use(TreeSelect);
+Vue.use(Uploader);
