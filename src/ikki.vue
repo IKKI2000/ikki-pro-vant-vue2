bea2896e9871d3006e8dc84cb78887a3b9728818
IKKI2000 <ikki2002@qq.com>
2023-02-12 21:55:38 +0800

    【删除】脚手架演示文件~

--- src/ikki.vue
+++ src/ikki.vue
@@ -1,29 +1,20 @@
 <template>
-    <div id="app">
+    <div id="ikki">
         <van-overlay :show="$store.state.loading">
             <van-loading vertical type="spinner" :color="$utils.less.primaryColor" size="60" :text-color="$utils.less.primaryColor" text-size="24">{{ $t('Common.Loading') }}</van-loading>
         </van-overlay>
-        <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </nav>
         <router-view />
     </div>
 </template>
 
 <script>
     export default {
-        name: 'AppView',
+        name: 'IKKIView',
     };
 </script>
 
 <style lang="less">
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
+    #ikki {
         > .van-overlay {
             display: flex;
             justify-content: center;
@@ -46,18 +37,6 @@
                 border-left-color: @green;
                 width: 12px;
                 height: 12px;
-            }
-        }
-    }
-    nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
             }
         }
     }
